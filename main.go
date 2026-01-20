0a127902f6a84607a82164bc92f4ad7375ba1a7d
farrasnazhif <farrasnazhif@gmail.com>
2026-01-20 18:03:42 +0700

    feat: golang

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	controller "github.com/farrasnazhif/moviestream-go/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, day 1 learn go!")
+	})
+
+	router.GET("/api/movies", controller.GetMovies())
+	router.GET("/api/movie/:imdb_id", controller.GetMovieByID())
+	router.POST("/api/movies", controller.AddMovie())
+
+	//	err != nil --> error, nill is like null in javascript, so if its not null == error
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server, err")
+	}
+
+}
