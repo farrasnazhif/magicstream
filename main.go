50c90f8043f9b2f5a2ed1c25c561051ba5f7b466
farrasnazhif <farrasnazhif@gmail.com>
2026-01-21 07:13:54 +0700

    feat: setup middleware routes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/farrasnazhif/moviestream-go/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,9 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, day 1 learn go!")
 	})
+
+	routes.UnprotectedRoutes(router)
+	routes.ProtectedRoutes(router)
 
 	//	err != nil --> error, nill is like null in javascript, so if its not null == error
 	if err := router.Run(":8080"); err != nil {
